6b3d0367523cb12d019ca3b7e321a67c93de8ec3
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2020-11-21 22:57:23 +0200

    solve 8 Q

---
+++ Q2/main (2).js
@@ -0,0 +1,30 @@
+console.log('Problem Solving Q: 2 ');
+
+/* Longest Word 2 */
+
+/*  
+Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
+*/
+
+function longestWord_2(sen) {
+  // YOUR CODE HERE
+  const newArry=sen.split(" ")
+  let longWord=newArry[0]
+  for(let index=0 ; index<newArry.length;index++)
+  {
+    if(index===newArry.length-1){
+return longWord
+    } if(newArry[index].length>newArry[index+1].length){ 
+      longWord=newArry[index]
+     }else
+     longWord=newArry[index+1]
+  }
+}
+
+/* 
+Examples:
+longestWord_2('lol!- time.'); // => 'time'
+longestWord_2('I** love, cats'); // => 'love'
+longestWord_2('coding&& is awesome'); // => 'awesome'
+longestWord_2('hello|| world'); // => 'hello'
+*/
