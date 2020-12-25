a661a05213f01b067cd89501109ae9fc3a9818a0
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2020-12-25 16:03:35 +0200

    solve Q4

--- theard groub/02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ theard groub/02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const ibj={}
+    let max=0
+    let maxChar=' '
+    for(let i=0;i<str.length;i++){
+        if(obj[str[i]]){
+            obj[str[i]]++
+        }else obj[str[i]]=1
+    }
+    for (let char in obj) {
+        if (obj[char] > max) {
+          max = obj[char];
+          maxChar = char;
+        }
+      }
+    return maxChar
+}
 
 module.exports = maxChar;
