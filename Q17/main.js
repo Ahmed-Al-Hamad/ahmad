7787b79a8bac18a0477b00f24964e6f458820be4
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2021-01-13 15:41:13 +0200

    solve Q17 from Q26

--- Q17/main.js
+++ Q17/main.js
@@ -6,8 +6,18 @@ console.log('Problem Solving Q:17 ');
 Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'
 */
 
-function firstNonRepeatedCharacter() {
+function firstNonRepeatedCharacter(str) {
   // YOUR CODE HERE
+  const obj={}
+  let newArr=str.split("")
+    for(let i=0;i<newArr.length;i++){
+      obj[newArr[i]]=1+(obj[newArr[i]]||0)
+    }
+    for(k in obj){
+      if(obj[k]===1){
+        return k
+      }
+    }
 }
 
 /* 
