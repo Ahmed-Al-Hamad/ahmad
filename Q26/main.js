6f3ee1e1ff588376ebfc418072b579aa9ee3bd15
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2021-01-14 15:21:30 +0200

    solve Q26 from Q26

--- Q26/main.js
+++ Q26/main.js
@@ -6,8 +6,10 @@ console.log('Problem Solving Q:26 ');
 Write a JavaScript function to clone an array.
 */
 
-function array_Clone() {
+function array_Clone(arr) {
   // YOUR CODE HERE
+  let array=[...arr]
+  return array
 }
 
 /* 
