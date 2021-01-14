fe7c5b41c2d014c0fdb2220cfdfdd04dad4c67f5
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2021-01-14 13:11:36 +0200

    solve Q21 from Q26

--- Q21/main.js
+++ Q21/main.js
@@ -6,8 +6,19 @@ console.log('Problem Solving Q: 21');
 Given a string, remove any characters that are not unique from the string.
 */
 
-function onlyUnique() {
+function onlyUnique(str) {
   // YOUR CODE HERE
+    const obj ={}
+    let strUnique=''
+    for(let i=0; i<str.length;i++){
+        obj[str[i]]=1+(obj[str[i]]||0)
+    }
+    for(k in obj){
+      if(obj[k]==1){
+        strUnique+=obj[k]
+      }
+    }
+    return strUnique
 }
 
 /* 
