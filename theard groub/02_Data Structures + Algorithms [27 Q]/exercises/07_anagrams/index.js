eb3456bb20852749dbd132def3c483eb4f55cb65
Ahmed-Al-Hamad <alhamad.m.ahmed@gmail.com>
2021-01-19 13:42:41 +0200

    solve Q7 FROM Q27

--- theard groub/02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
+++ theard groub/02_Data Structures + Algorithms [27 Q]/exercises/07_anagrams/index.js
@@ -7,7 +7,26 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+/* replace(/[^\w]/g, '') */
+function anagrams(stringA, stringB) {
+  const objA=factObj(stringA.replace(/[^\w]/g, '').toLowerCase())
+  const objB=factObj(stringB.replace(/[^\w]/g, '').toLowerCase())
+  if(Object.keys(objA).length!==Object.keys(objB).length){
+      return false;
+  }
+  for(let k in objA){
+      if(objA[k]!==objB[k]){
+         return false
+      }
+  }
+  return true
+}
 
-function anagrams(stringA, stringB) {}
-
+function factObj(str){
+    const obj={}
+    for(let i=0; i<str.length;i++){
+        obj[str[i]]=obj[i]+1||1
+    }
+    return obj
+}
 module.exports = anagrams;
